36d458fe966cef30a30ef8fcdd233634730979be
tmckinney <tmckinney121@gmail.com>
2017-12-09 13:59:15 -0500

    Wrote defaultServerRenderer method for composable server-rendered components

---
+++ App/boot-server.tsx
@@ -0,0 +1,25 @@
+import * as React from 'react';
+import { renderToString } from 'react-dom/server';
+import { createServerRenderer, RenderResult } from 'aspnet-prerendering';
+
+type Globals = {
+  [index: string]: any
+}
+
+const defaultServerRenderer = (Element: any, name: string) => createServerRenderer(params => {
+  const globals: Globals = {};
+  globals[name] = params.data;
+
+  return new Promise<RenderResult>((resolve, reject) => {
+    resolve({
+      html: renderToString(<Element {...params.data} />),
+      globals
+    });
+  });
+});
+
+import  App from './_App';
+
+module.exports = {
+  App: defaultServerRenderer(<App />, 'app')
+}
