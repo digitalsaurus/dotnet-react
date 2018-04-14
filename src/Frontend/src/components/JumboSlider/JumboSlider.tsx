319dd36acc80ecc96eefabc0880484ad147ead06
Tom McKinney <tmckinney121@gmail.com>
2018-04-14 18:05:07 -0400

    Added comprehensive flexbox styling for JumboSlider

--- src/Frontend/src/components/JumboSlider/JumboSlider.tsx
+++ src/Frontend/src/components/JumboSlider/JumboSlider.tsx
@@ -16,13 +16,27 @@ export default class JumboSlider extends React.Component<JumboSliderProps> {
   render() {
     return (
       <section className="jumbo-slider">
-        <ul className="jumbo-slider__slide-list">
+        <div className="jumbo-slider__scroller">
+          <ul>
+            {this.props.slides && this.props.slides.map((slide, i) => (
+              <li key={i}>
+                <img src={slide.image} />
+              </li>
+            ))}
+          </ul>
+        </div>
+        {/* {this.props.slides && this.props.slides.map((slide, i) => (
+          <figure key={i}>
+            <img src={slide.image} />
+          </figure>
+        ))} */}
+        {/* <ul className="jumbo-slider__slide-list">
           {this.props.slides && this.props.slides.map((slide, i) => (
             <li key={i}>
               <img src={slide.image} />
             </li>
           ))}
-        </ul>
+        </ul> */}
       </section>
     );
   }
