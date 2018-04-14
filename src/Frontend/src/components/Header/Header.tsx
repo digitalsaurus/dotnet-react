ed720a3d6a8ecdd040403d654264409dc40360da
Tom McKinney <tmckinney121@gmail.com>
2018-04-14 15:50:18 -0400

    Header is now fixed with psuedo-element padding on viewport

--- src/Frontend/src/components/Header/Header.tsx
+++ src/Frontend/src/components/Header/Header.tsx
@@ -1,12 +1,12 @@
 import * as React from 'react';
-import { NavLink } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 import './Header.css';
 
 const Header: React.SFC = () => (
   <header className="site-header">
     <div className="site-header__navbar">
       <div className="container">
-        <a href="#" className="site-header__navbar__logo">Dotnet-React</a>
+        <Link to="/" className="site-header__navbar__logo">Dotnet-React</Link>
         <nav>
           <NavLink to="/">Home</NavLink>
           <NavLink to="/dogs">Dogs</NavLink>
